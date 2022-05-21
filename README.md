03f5bcac0e010fb20011de54543fa63c12a3df93
caspianare <caspianare@gmail.com>
2022-05-21 02:51:06 -0700

    Use webpack

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 
 ## Run Tests
 ```npm test```
+
+## Build for development
+```npm run build-dev```
+This will generate an unminimized dist/qatool.js with source maps.
+
+## Build for release
+```npm run build```
+This will generate a minified dist/qatool.js.
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/qatool.ts',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'qatool.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
